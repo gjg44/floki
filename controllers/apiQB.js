2da1560cf12dd1d447e27a9caf0cdddf663f3dec
Greg Gerst <greg@rrjanitorial.com>
2017-09-22 22:57:19 -0700

    added async/await functionality to QB endpoints.

--- controllers/apiQB.js
+++ controllers/apiQB.js
@@ -35,51 +35,57 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true}));
    
   // go to QB auth page
-  app.get('/authQB', (req, res) => {
+  app.get('/authQB', async (req, res) => {
     console.log('entering /authQB endpoint');
-    res.redirect(qbAuthURI);
-    if (res.statusMessage === 'Found') {
-      console.log(`Redirecting to QBO Auth Page`);
-    } else {
-      console.log(res.error);
+    try {
+      res.redirect(qbAuthURI);
+      if (res.statusMessage === 'Found') {
+        console.log(`Redirecting to QBO Auth Page`);
+      } else {
+        throw `Error communicating with Intuit's oAuth2 Server: ${res.error}`;
+      }
+    } catch(e) {
+      return res.json(e);
     }
     console.log('exiting /authQB endpoint');
   });
 
   // test account access
-  app.get('/testChartofAccountsAccess', (req,res) => {
+  app.get('/testChartofAccountsAccess', async (req,res) => {
     console.log('entering /testChartofAccountsAccess endpoint');
-
-    var qbo = new QuickBooks(nodeQBObj);
-    qbo.findAccounts(function(_, accounts) {
-      res.json(accounts.QueryResponse);
-    });
-
+    try {
+      var qbo = new QuickBooks(nodeQBObj);
+      await qbo.findAccounts(function(_, accounts) {
+        res.json(accounts.QueryResponse);
+      });
+    } catch (e) {
+      return res.json(e);
+    }
     console.log('exiting /testChartofAccountsAccess endpoint');
   });
 
   // test account access
-  app.get('/writeEmployeesToDatabase', (req,res) => {
+  app.get('/writeEmployeesToDatabase', async (req,res) => {
     console.log('entering /writeEmployeesToDatabase endpoint');
-
-    var qbo = new QuickBooks(nodeQBObj);
-    qbo.findEmployees(function(_, employees) {
-      employees.QueryResponse.Employee.forEach(function(employee) {
-        //Check if employee is already in the database. If so, update. If not, add new employee entry to database.  Check based on 'DisplayName'.
-        var query = {'DisplayName': employee.DisplayName};
-        QBEmployee.findOneAndUpdate(query,employee,{upsert:true}, function(err,doc) {
-          if(err) console.log(`findOneAndUpdate error: ${err}`);
-          if(doc) {
-            console.log(`${employee.DisplayName}'s employee record updated with new information.`);
-          } else {
-            console.log(`${employee.DisplayName}'s employee record not found. Adding to database.`);
-          }
-        });      
-      })
-//      res.json(employees.QueryResponse);
+    try {
+      var qbo = new QuickBooks(nodeQBObj);
+      await qbo.findEmployees(function(_, employees) {
+        employees.QueryResponse.Employee.forEach(function(employee) {
+          var query = {'DisplayName': employee.DisplayName};
+          QBEmployee.findOneAndUpdate(query,employee,{upsert:true}, function(err,doc) {
+            if(err) throw `findOneAndUpdate error: ${err}`;
+            if(doc) {
+              console.log(`${employee.DisplayName}'s employee record updated with new information.`);
+            } else {
+              console.log(`${employee.DisplayName}'s employee record not found. Adding to database.`);
+            }
+          });      
+        });
       return res.redirect('/');
-    });
-
+      });
+    } catch (e) {
+      return res.json(e);
+    }
     console.log('exiting /writeEmployeesToDatabase endpoint');
   });
 
